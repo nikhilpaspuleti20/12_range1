07576330ef2fa4a9e5fc28f2fe1cff2ab7b71ca9
nikhilpaspuleti20 <116423567+nikhilpaspuleti20@users.noreply.github.com>
2022-12-02 22:28:50 +0530

    Add files via upload

---
+++ range1.py
@@ -0,0 +1,13 @@
+'''
+DAY:WEDNESDAy
+DATE:30th NOV 2022
+TOPIC:range
+AUTHOR:pooja
+'''
+b=5
+for i in range(0,b):
+  print(i**2)# 0
+              #1
+              #4
+              #9
+              #16
